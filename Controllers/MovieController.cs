7da05b80a22731e45b40d0252853e4bf207436cc
Wilson <dreamy@Adryennes-MBP.attlocal.net>
2020-07-20 21:57:18 -0400

    Commit to add original Code

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -2,14 +2,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab_22.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab_22.Controllers
 {
     public class MovieController : Controller
     {
+        public static List<Movie> Movies = new List<Movie>
+        {
+          new Movie("Halloween", "Horror"),
+          new Movie("Cool Runnings", "Sci-Fi"),
+          new Movie("White Chicks", "Comedy"),
+          new Movie("Scary Movie", "Ramance"),
+          new Movie("Friday", "Drama")
+        };
+
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult SearchResultGenre(string genre)
+        {
+            List<Movie> genreList = new List<Movie>();
+
+            foreach (Movie movie in Movies)
+            {
+                if (movie.Genre == genre)
+                {
+                    genreList.Add(movie);
+                }
+            }
+            return View(genreList);
+        }
+        public IActionResult SearchResultTitle(string title)
+        {
+            foreach (Movie movie in Movies)
+            {
+                if (movie.Title.ToLower() == title.ToLower())
+                {
+                    return View(movie);
+                }
+
+            }
+
             return View();
         }
     }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,8 +3,18 @@ namespace Lab_22.Models
 {
     public class Movie
     {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
         public Movie()
         {
+
+        }
+
+        public Movie(string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
         }
     }
 }
